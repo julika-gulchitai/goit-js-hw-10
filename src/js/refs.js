822aad8af73524244108e27a116093621816b9e0
julika-gulchitai <127190696+julika-gulchitai@users.noreply.github.com>
2023-11-15 12:25:28 +0200

    hw10-1

--- src/js/refs.js
+++ src/js/refs.js
@@ -2,3 +2,10 @@ export const selectEl = document.querySelector('.breed-select');
 export const errorMessage = document.querySelector('.error');
 export const loaderMessage = document.querySelector('.loader');
 export const catInfo = document.querySelector('.cat-info');
+// import SlimSelect from 'slim-select';
+
+// console.log(selectEl);
+// selectEl = new SlimSelect({
+//   select: '#selectElement'
+// })
+// console.log(selectEl);
